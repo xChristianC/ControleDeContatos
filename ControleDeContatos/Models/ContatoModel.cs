0c20535b0b31bdc06a88df2d9655197982cec242
Christian Conceição <christian.c664@gmail.com>
2024-06-25 06:26:39 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel : Controller
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+    }
+}
